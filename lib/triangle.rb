f02f3ff34e8b45b9fac0286416634d953cb1138d
Clyde Griffin <deezy13@gmail.com>
2019-12-04 04:50:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,21 @@ class Triangle
   def kind()
     if @equilateral <= 0 || @isosceles <= 0 || @scalene <= 0 
       raise TriangleError
-      
+       elsif (@equilateral  + @isosceles <= @scalene) || (@equilateral + @scalene <= @isosceles) || (@isosceles+ @scalene <= @equilateral)
+      raise TriangleError
+    else
+      if (@equilateral == @isosceles) && (@isosceles == @scalene)
+        :equilateral
+      elsif (@equilateral == @isosceles) || (@isosceles == @scalene) || (@equilateral == @scalene)
+        :isosceles
+      elsif (@equilateral != @isosceles) && (@isosceles != @scalene) && (@equilateral != @scalene)
+        :scalene
+      end
+    end
+
+  end
+
+end
   end 
   
   class TriangleError < StandardError
