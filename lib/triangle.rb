8b0ec816f6cbeda93044553ca6b37a02906c088d
Clyde Griffin <deezy13@gmail.com>
2019-12-04 04:38:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
   # write code here
+  
+  def initialize(equilateral, isosceles, scalene)
+    
+    
+  end 
+  
+  def kind(equilateral)
+    
+    equilateral
+  end 
+  
+  class TriangleError < StandardError
+    
+    
+    
+  end 
 end
